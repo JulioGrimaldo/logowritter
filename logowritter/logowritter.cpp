2a91464eaf1cdc096796704ed62da8a6fdc41ad4
Julio Cesar Grimaldo Figueroa <al230250@edu.uaa.mx>
2022-12-08 21:27:05 -0600

    Build 1.0

--- logowritter/logowritter.cpp
+++ logowritter/logowritter.cpp
@@ -7,7 +7,7 @@
 
 
 using namespace std;
-
+static int varX, varY = 10;
     void gotoxy(int x, int y) {
         HANDLE hcon;
         hcon = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -16,11 +16,82 @@ using namespace std;
         dwPos.Y = y;
         SetConsoleCursorPosition(hcon, dwPos);
     }
-    void comparar_seleccion(string comando, string numero) {
-        int res = toupper(comando.compare("movr"));
+    void coordenadas(int &varX, int &varY) {
+        gotoxy(100, 0);
+        cout << "                    " << endl;
+        gotoxy(100, 0);
+        cout << "Coordenadas: " << varX << "," << varY<<endl;
+    }
+    void comparar_seleccion(string comando, int num) {
+        int res;
+        res = toupper(comando.compare("MOVR"));
         if (res == 0) {
-            int num = stoi(numero); //String to int 
-            cout << "movr seleccionado"<<endl;
+            int var = varX + num;
+            if (var > 112) {
+                gotoxy(0, 8);
+                cout << "Numero fuera de rango" << endl;
+                system("pause");
+            }
+            else {
+            for (int i = 0; i < num; i++) {
+
+                coordenadas(varX, varY);
+                gotoxy(varX, varY);
+                cout << "*";
+                varX++;
+            }
+        }
+        }
+        res = toupper(comando.compare("MOVD"));
+        if (res == 0) {
+            int var = varY + num;
+            if (var > 28) {
+                gotoxy(0, 8);
+                cout << "Numero fuera de rango" << endl;
+                system("pause");
+            }
+            else {
+            for (int i = 0; i < num; i++) {
+                coordenadas(varX, varY);
+                gotoxy(varX, varY);
+                cout << "*";
+                varY++;
+            }
+        }
+        }
+        res = toupper(comando.compare("MOVL"));
+        if (res == 0) {
+            int var = varX - num;
+            if (var < 10) {
+                gotoxy(0, 8);
+                cout << "Numero fuera de rango" << endl;
+                system("pause");
+            }
+            else {
+                for (int i = 0; i < num; i++) {
+                    coordenadas(varX, varY);
+                    gotoxy(varX, varY);
+                    cout << "*";
+                    varX--;
+                }
+            }
+        }
+        res = toupper(comando.compare("MOVU"));
+        if (res == 0) {
+            int var = varY - num;
+            if (var < 10) {
+                gotoxy(0, 8);
+                cout << "Numero fuera de rango" << endl;
+                system("pause");
+            }
+            else {
+                for (int i = 0; i < num; i++) {
+                    coordenadas(varX, varY);
+                    gotoxy(varX, varY);
+                    cout << "*";
+                    varY--;
+                }
+            }
         }
 
 
@@ -43,39 +114,45 @@ using namespace std;
         }
         else {
             transform(comando.begin(), comando.end(), comando.begin(), ::toupper);
-            comparar_seleccion(comando, numero);
-            cout << "Comando: " << comando << endl;
-            cout << "Numero: " << numero << endl;
+            int num = stoi(numero);
+            comparar_seleccion(comando, num);
         }
-        
         return com;
     }
+
     void Menu() {
-        cout << "Use:" << endl;
+        cout << "Comandos:" << endl;
         cout << "Movl" << endl;
         cout << "Movr" << endl;
         cout << "Movu" << endl;
         cout << "Movd" << endl;
+        cout << "q Para salir" << endl;
+        cout << "Utilice la siguiente estructura <comando>, <num>." << endl;
     }
     
     int main() {
         Menu();
         string beffa;
-        
         bool res;
+        int resu=1;
         do {
+            coordenadas(varX, varY);
+            gotoxy(0, 9);
+            cout << "                                                    " << endl;
+            gotoxy(0, 8);
             getline(cin, beffa);
-            res=separar_entrada(beffa);
-        } while (res ==false);
+             resu= toupper(beffa.compare("q"));
+            if (resu == 0) {
+            }
+            else {
 
-        
-        return 0;
+                do {
+                    res = separar_entrada(beffa);
+                } while (res == false);
+            }
+            gotoxy(0, 8);
+            cout << "                       " << endl;
+            } while (resu != 0);
+            
+    return 0;
     }
-
-
-
-
-
-
-
-
